1d2c39c209c0c69e9b395d863de9241f3ef21f62
John Sabilla <jsabilla@ucsd.edu>
2014-12-11 00:43:25 -0800

    refactored how entries are computed. made a separate method for it outside of client side to make app more secure

--- ProteinTracker.js
+++ ProteinTracker.js
@@ -1,19 +1,53 @@
-Users = new Meteor.Collection('users');
+ProteinData = new Meteor.Collection('protein_data');
 History = new Meteor.Collection('history');
 
+Meteor.methods({
+  addProtein: function(amount, userId){
+      console.log('addProtein: ', amount);
+      ProteinData.update({userId : userId}, { $inc : { total : amount } } );
+        History.insert({
+            value: amount,
+            date: new Date().toTimeString(),
+            userId: userId
+        });
+  }
+
+});
+
+
 if (Meteor.isClient) {
-    Meteor.subscribe('allUsers');
+    Meteor.subscribe('allProteinData');
     Meteor.subscribe('allHistory');
     
+    Deps.autorun( function(){
+      if(Meteor.user()){
+          console.log('User: ' + Meteor.user().profile.name + ' is logged in');
+      }
+      else{
+          console.log('User logged out');
+      }
+    });
     Template.userDetails.helpers({
       user: function() {
-          return Users.findOne();
+           var data = ProteinData.findOne();
+           if(!data) {
+              data = {
+                  userId: Meteor.userId(),
+                  total: 0,
+                  goal: 200
+              };
+              ProteinData.insert(data);
+           }
+           return data;
+      },
+      lastAmount: function () {
+          return Session.get('lastAmount');
       }
     });
 
     Template.history.helpers({
       historyItem: function() {
-          return History.find({}, {sort: {date: -1}, limit: 5});
+          return History.find({},{sort: {date: -1}});
       }
     });
 
@@ -21,56 +55,32 @@ if (Meteor.isClient) {
       'click #addAmount' : function(e){
         e.preventDefault();
         var amount = parseInt( $('#amount').val() );
-        Users.update(this._id, {$inc: {total: amount } } );
-        History.insert({
-            value: amount,
-            date: new Date().toTimeString(),
-            userId: this._id
+       
+        Meteor.call('addProtein', amount, this.userId, function(error, id){
+          if(error)
+            return alert(error.reason);
         });
+        Session.set('lastAmount', amount);
+        //console.log('lastAmount', Session.get('lastAmount'));
       }
     });
 }
 
 if (Meteor.isServer) {
 
-  Meteor.publish('allUsers', function() {
-    return Users.find();
+  Meteor.publish('allProteinData', function() {
+    return ProteinData.find({ userId: this.userId });
   });
 
   Meteor.publish('allHistory', function() {
-    return History.find();
+    return History.find({ userId: this.userId }, { sort:{ date: -1 } , limit: 5 } );
   });
 
   Meteor.startup(function () {
     // code to run on server at startup
 
-    if(Users.find().count() === 0){
-      Users.insert({ 
-        total: 120,
-        goal: 200
-      });
-    }
-    if(History.find().count() === 0) {
-      History.insert({
-        value: 5,
-        date: new Date().toTimeString()
-      });
-      History.insert({
-        value: 10,
-        date: new Date().toTimeString()
-      });
-      History.insert({
-        value: 15,
-        date: new Date().toTimeString()
-      });
-      History.insert({
-        value: 20,
-        date: new Date().toTimeString()
-      });
-      History.insert({
-        value: 25,
-        date: new Date().toTimeString()
-      });
-    }
+    //Uncomment to clear History collection
+    //History.remove({});
+    //ProteinData.remove({});
   });
 }
